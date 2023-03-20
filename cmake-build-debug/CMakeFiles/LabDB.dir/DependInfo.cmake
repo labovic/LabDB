23db93ceedda48440a7d242025f0eb057ded714e
SteLab <labovicstefan00@gmail.com>
2023-03-20 18:09:34 +0300

    Added Report

---
+++ cmake-build-debug/CMakeFiles/LabDB.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Stefan/CLionProjects/LabDB/database.c" "CMakeFiles/LabDB.dir/database.c.o" "gcc" "CMakeFiles/LabDB.dir/database.c.o.d"
+  "/mnt/c/Users/Stefan/CLionProjects/LabDB/main.c" "CMakeFiles/LabDB.dir/main.c.o" "gcc" "CMakeFiles/LabDB.dir/main.c.o.d"
+  "/mnt/c/Users/Stefan/CLionProjects/LabDB/pager.c" "CMakeFiles/LabDB.dir/pager.c.o" "gcc" "CMakeFiles/LabDB.dir/pager.c.o.d"
+  "/mnt/c/Users/Stefan/CLionProjects/LabDB/query.c" "CMakeFiles/LabDB.dir/query.c.o" "gcc" "CMakeFiles/LabDB.dir/query.c.o.d"
+  "/mnt/c/Users/Stefan/CLionProjects/LabDB/table.c" "CMakeFiles/LabDB.dir/table.c.o" "gcc" "CMakeFiles/LabDB.dir/table.c.o.d"
+  "/mnt/c/Users/Stefan/CLionProjects/LabDB/utils.c" "CMakeFiles/LabDB.dir/utils.c.o" "gcc" "CMakeFiles/LabDB.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
